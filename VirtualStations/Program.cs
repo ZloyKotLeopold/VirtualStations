9f1725c402e66c532cb1780b4adb3ff7890e9194
ZloyKotLeopold <140909093+ZloyKotLeopold@users.noreply.github.com>
2024-02-25 13:24:08 +0600

    add constructor

--- VirtualStations/Program.cs
+++ VirtualStations/Program.cs
@@ -18,29 +18,56 @@ namespace VirtualStations
     public class Car
     {
         private Passenger[] _passengers;
+
+        public Car(Passenger[] passengers)
+        {
+            _passengers = passengers;
+        }
     }
 
     public class Train
     {
         private Car[] _cars;
+
+        public Train(Car[] cars)
+        {
+            _cars = cars;
+        }
     }
 
     public class Passenger
     {
         private int _money;
         private Ticket _ticket;
+
+        public Passenger(int money)
+        {
+            _money = money;
+        }
     }
 
     public class Route
     {
         private string _directed;
         private Train _train;
+
+        public Route(string directed, Train train)
+        {
+            _directed = directed;
+            _train = train;
+        }
     }
 
     public class Ticket
     {
         private int _price;
         private Route _route;
+
+        public Ticket(int price, Route route)
+        {
+            _price = price;
+            _route = route;
+        }
     }
 
 }
